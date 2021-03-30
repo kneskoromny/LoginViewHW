a0a944c73a5ee04e2dd1c855992e7bd427b29594
kneskoromny <kneskoromny@gmail.com>
2021-03-30 08:45:31 +0300

    Initial Commit

---
+++ LoginView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginView
+//
+//  Created by Кирилл Нескоромный on 30.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
