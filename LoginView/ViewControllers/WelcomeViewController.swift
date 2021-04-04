76893a18fda713d7516bad6032db5f4ca06e8ff3
Kirill Neskoromny <kneskoromny@gmail.com>
2021-04-04 20:21:30 +0300

    fixed mistakes after check

--- LoginView/ViewControllers/WelcomeViewController.swift
+++ LoginView/ViewControllers/WelcomeViewController.swift
@@ -16,6 +16,7 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         gradientLayer()
         welcomeLabel.text = "Добро пожаловать, \(currentUser.person.name) \(currentUser.person.surname)!"
     }
